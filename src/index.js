b96f65f8604353a97e94930af4187cf70f5c28c2
JP <jpylisela@gmail.com>
2017-02-13 00:22:24 +0000

    minor

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react'
-//import ReactDOM from 'react-dom';
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import reducerState from './reducers'
 
-// import Application from './Application';
+import reducerState from './reducers'
 import Application from './components';
 import './index.css';
 
-let store = createStore(reducerState)
-
+let store = createStore(reducerState);
 
 render(
 	<Provider store={store}>
